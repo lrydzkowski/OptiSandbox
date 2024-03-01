a0a106d4bc1b4a74265f0276c0e372572952e5b9
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-03-01 19:25:46 +0100

    feat: handle default page controller

---
+++ OptiSandbox/Controllers/DefaultPageController.cs
@@ -0,0 +1,25 @@
+﻿using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using OptiSandbox.Models;
+
+namespace OptiSandbox.Controllers;
+
+[TemplateDescriptor(Inherited = true)]
+public class DefaultPageController : PageController<SitePageData>
+{
+    public ViewResult Index(SitePageData currentPage)
+    {
+        IPageViewModel<SitePageData>? model = CreateModel(currentPage);
+
+        return View($"~/Views/{currentPage.GetOriginalType().Name}/Index.cshtml", model);
+    }
+
+    private static IPageViewModel<SitePageData>? CreateModel(SitePageData page)
+    {
+        Type type = typeof(PageViewModel<>).MakeGenericType(page.GetOriginalType());
+        object? instance = Activator.CreateInstance(type, page);
+
+        return instance as IPageViewModel<SitePageData>;
+    }
+}
