9aa42c8829f1d836a0ebb245655378f02dd0d1c9
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-03-02 18:12:20 +0100

    feat: add view for banner block

---
+++ OptiSandbox/Components/BannerBlockViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using OptiSandbox.Models.Blocks;
+
+namespace OptiSandbox.Components;
+
+public class BannerBlockViewComponent : BlockComponent<BannerBlock>
+{
+    protected override IViewComponentResult InvokeComponent(BannerBlock currentContent)
+    {
+        return View(currentContent);
+    }
+}
