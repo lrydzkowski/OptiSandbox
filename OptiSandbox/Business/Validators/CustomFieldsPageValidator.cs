112e4ce0413bee5ae108421d3695f6f42667a60e
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-03-16 11:35:41 +0100

    feat: add simple content save validator

---
+++ OptiSandbox/Business/Validators/CustomFieldsPageValidator.cs
@@ -0,0 +1,29 @@
+﻿using EPiServer.Validation;
+using OptiSandbox.Models.Pages;
+
+namespace OptiSandbox.Business.Validators;
+
+public class CustomFieldsPageValidator : IContentSaveValidate<CustomFieldsPage>
+{
+    public IEnumerable<ValidationError> Validate(CustomFieldsPage instance, ContentSaveValidationContext context)
+    {
+        List<ValidationError> validationErrors = new();
+        string valueToFind = "Value2";
+        if (instance.SingleValue?.Equals(valueToFind, StringComparison.InvariantCultureIgnoreCase) == true
+            && instance.MultipleValues?.Count > 0
+            && instance.MultipleValues[0].Contains(valueToFind, StringComparison.InvariantCultureIgnoreCase))
+        {
+            validationErrors.Add(
+                new ValidationError
+                {
+                    Severity = ValidationErrorSeverity.Error,
+                    ErrorMessage = "Wrong values :)",
+                    RelatedProperties = new List<string>
+                        { nameof(instance.SingleValue), nameof(instance.MultipleValues) }
+                }
+            );
+        }
+
+        return validationErrors;
+    }
+}
