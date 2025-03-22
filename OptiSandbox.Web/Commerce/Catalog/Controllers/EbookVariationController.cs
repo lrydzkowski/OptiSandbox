9d817316cc779f728dc14f976be91595e20a88df
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2025-03-22 18:08:45 +0100

    feat: unify view between products and variants

--- OptiSandbox.Web/Commerce/Catalog/Controllers/EbookVariationController.cs
+++ OptiSandbox.Web/Commerce/Catalog/Controllers/EbookVariationController.cs
@@ -1,21 +1,32 @@
+using EPiServer.Commerce.Catalog.ContentTypes;
 using EPiServer.Web.Mvc;
 using Microsoft.AspNetCore.Mvc;
+using OptiSandbox.Web.Commerce.Catalog.Models.Products;
 using OptiSandbox.Web.Commerce.Catalog.Models.Variations;
-using OptiSandbox.Web.Content.Services;
+using OptiSandbox.Web.Commerce.Catalog.Services;
 
 namespace OptiSandbox.Web.Commerce.Catalog.Controllers;
 
 public class EbookVariationController : ContentController<EbookVariation>
 {
-    private readonly IPageViewModelBuilder _pageViewModelBuilder;
+    private readonly IContentLoader _contentLoader;
 
-    public EbookVariationController(IPageViewModelBuilder pageViewModelBuilder)
+    private readonly IEbookProductViewModelBuilder _ebookProductViewModelBuilder;
+
+    public EbookVariationController(
+        IContentLoader contentLoader,
+        IEbookProductViewModelBuilder ebookProductViewModelBuilder
+    )
     {
-        _pageViewModelBuilder = pageViewModelBuilder;
+        _contentLoader = contentLoader;
+        _ebookProductViewModelBuilder = ebookProductViewModelBuilder;
     }
 
     public IActionResult Index(EbookVariation currentContent)
     {
-        return View(_pageViewModelBuilder.Build(currentContent));
+        ContentReference? contentReference = currentContent.GetParentProducts()?.FirstOrDefault();
+        EbookProduct product = _contentLoader.Get<EbookProduct>(contentReference);
+
+        return View("~/Commerce/Catalog/Views/EbookProduct/Index.cshtml", _ebookProductViewModelBuilder.Build(product));
     }
 }
